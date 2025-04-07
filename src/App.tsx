eca466b837f883c423c76270b31d26db261ccedf
tarpeeton <tarpeetonteam@gmail.com>
2025-04-07 10:31:51 +0300

    build error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/navbar";
 import Advantage from "./sections/advantage";
-import Contact from "./sections/contact";
+// import Contact from "./sections/contact";
 import Endocrina from "./sections/endocrina";
 import Faq from "./sections/faq";
 import Footer from "./sections/footer";
 import Hero from "./sections/hero";
 import Instruction from "./sections/instruction";
-import MyInfo from "./sections/my-info";
+// import MyInfo from "./sections/my-info";
 import Onlineonsultation from "./sections/online-consultation";
 import Recommunded from "./sections/recommunded";
 import Reviews from "./sections/reviews";
